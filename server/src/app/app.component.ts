bc22ea352b0d5426099aab14d758591df236cc7c
Chaitanya Babar <ChaitanyaBabar@users.noreply.github.com>
2025-05-02 11:59:09 +0530

    Demo changes | PR Check the github Action for check-copyright.yml

--- server/src/app/app.component.ts
+++ server/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularApp';
+
+  /**
+   * Demo change in the app.component.ts file
+   * to test the GitHub Actions workflow
+   * for rasing a PR , containing the file that does not
+   * have copy-right headers and hence Github Actions
+   * for check-copyright.yml make the PR to fail.
+   */
 }
