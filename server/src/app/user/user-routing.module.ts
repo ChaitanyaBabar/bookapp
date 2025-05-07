43bd489e77bdc1be97eaee1713953f180a76c90e
Chaitanya Babar <ChaitanyaBabar@users.noreply.github.com>
2025-05-07 13:22:49 +0530

    Demo changes batch 4 | PR Check the github Action for check-copyright (#81)
    * Demo changes batch 3 | PR Check the github Action for check-copyright
    
    * fix: add/update missing copyright headers

--- server/src/app/user/user-routing.module.ts
+++ server/src/app/user/user-routing.module.ts
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2025 Chaitanya Babar
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information. 
+ */
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -8,4 +12,15 @@ const routes: Routes = [];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {
+
+ /**
+   * Demo change in the app.module.ts file
+   * to test the GitHub Actions workflow
+   * for rasing a PR , containing the file that does not
+   * have copy-right headers and hence Github Actions
+   * for check-copyright.yml make the PR to fail.
+   */
+
+
+ }
